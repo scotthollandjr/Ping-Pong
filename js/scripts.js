076d8fc7f76161a5e3ff7e0c6a47d1a32413a1a0
Scott Holland <scotthollandjr@gmail.com>
2016-02-15 09:14:15 -0800

    Add passing pong functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ var pingPong = function(original) {
   if ((original % 3 === 0) && (original % 5 != 0)) {
     return "ping";
   }
+  if ((original % 5 === 0) && (original % 3 != 0)) {
+    return "pong";
+  }
 }
