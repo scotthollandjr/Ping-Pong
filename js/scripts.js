e7cf1685bc33fb7a9a74bbc68e97a15bfbe534d4
Scott Holland <scotthollandjr@gmail.com>
2016-02-15 09:51:18 -0800

    Add initial user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-var pingPong = function(original) {
-  if ((original % 3 === 0) && (original % 5 != 0)) {
+var currentNum;
+var upper;
+
+var pingPong = function(currentNum) {
+  if ((currentNum % 3 === 0) && (currentNum % 5 != 0)) {
     return "ping";
-  } else if ((original % 5 === 0) && (original % 3 != 0)) {
+  } else if ((currentNum % 5 === 0) && (currentNum % 3 != 0)) {
     return "pong";
-  } else if ((original % 5 === 0) && (original % 3 === 0)) {
+  } else if ((currentNum % 5 === 0) && (currentNum % 3 === 0)) {
     return "ping pong";
   } else {
-    return original;
+    return currentNum;
   }
 }
+
+
+
+$(document).ready(function() {
+  $("form#entry").submit(function(event) {
+    upper = $("input#userEntry").val();
+
+    event.preventDefault();
+  });
+});
