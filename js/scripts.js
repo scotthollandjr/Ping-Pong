8ea80ff10770f7337a9e8b0e3c05a66d27f611f7
Scott Holland <scotthollandjr@gmail.com>
2016-02-15 09:18:36 -0800

    Add passing non-ping pong functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,7 @@ var pingPong = function(original) {
     return "pong";
   } else if ((original % 5 === 0) && (original % 3 === 0)) {
     return "ping pong";
+  } else {
+    return original;
   }
 }
