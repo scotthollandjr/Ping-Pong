5c7a2c1e1f3de015546f3d22b2e747ccabe865f4
Scott Holland <scotthollandjr@gmail.com>
2016-02-15 09:05:33 -0800

    Add initial spec template

---
+++ specs/specs.js
@@ -0,0 +1,10 @@
+describe('', function() {
+  it('', function() {
+    expect(()).to.equal("")
+  });
+});
+
+// return number divisible by 3 as ping
+// return number divisible by 5 as pong
+// return number divisible by 15 as ping pong
+// all other numbers returned as number
